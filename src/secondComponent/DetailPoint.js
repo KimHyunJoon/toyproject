a25f8b9076ff7c9ed3db99d389b878a60b1bfce4
KimHyunJoon <117712030+KimHyunJoon@users.noreply.github.com>
2023-01-20 14:59:03 +0900

    DetailPoint 진행중

--- src/secondComponent/DetailPoint.js
+++ src/secondComponent/DetailPoint.js
@@ -41,7 +41,229 @@ const Point = styled.div`
   }
 `;
 
+const TabWrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 90vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
 
+  nav {
+    margin-top: 10px;
+    height: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    ul {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+
+      li {
+        width: 33%;
+        height: 35px;
+        position: relative;
+        background: #f0f0f0;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        user-select: none;
+
+        .underline {
+          position: absolute;
+          bottom: 35px;
+          left: 0;
+          right: 0;
+          height: 1px;
+          background: ${(props) => props.theme.headerColor};
+        }
+        &.accumulate {
+          width: 22%;
+          border: 1px solid #f0f0f0;
+          margin: 5px;
+          border-radius: 5px;
+
+          &.selected {
+            border: 1px solid ${(props) => props.theme.mainColor};
+            color: ${(props) => props.theme.mainColor};
+          }
+        }
+        &.selected {
+          background: white;
+        }
+      }
+    }
+  }
+`;
+
+const AccumulateContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  flex-direction: column;
+  img {
+    width: 200px;
+    margin-bottom: 50px;
+  }
+  h1 {
+    margin-top: 20px;
+    display: flex;
+    font-weight: bolder;
+    font-size: 20px;
+
+    img {
+      margin-left: 20px;
+      width: 10px;
+    }
+  }
+`;
+
+function AccumulateContentNone() {
+  return (
+    <AccumulateContentWrapper>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/pointImg.png`} />
+      <h2>아직 리워드 내역이 없습니다.</h2>
+      <h2>오늘 뭐샀니 상품 구매하고 포인트 적립 받으세요!</h2>
+
+      <h1>
+        오늘 뭐샀니 사용법 확인하기
+        <span>
+          <img src={`${process.env.PUBLIC_URL}/assets/images/arrow.png`} />
+        </span>
+      </h1>
+    </AccumulateContentWrapper>
+  );
+}
+
+function Accumulate() {
+  const data = [
+    {
+      id: 0,
+      title: "예정",
+      description: <AccumulateContentNone />,
+    },
+    {
+      id: 1,
+      title: "완료",
+      description: <h1>헬로우 완료</h1>,
+    },
+    {
+      id: 2,
+      title: "불가",
+      description: <h1>헬로우 불가</h1>,
+    },
+    {
+      id: 3,
+      title: "취소",
+      description: <h1>헬로우 취소</h1>,
+    },
+  ];
+  const [index, setIndex] = useState(0);
+  return (
+    <TabWrapper>
+      <nav>
+        <ul>
+          {data.map((item) => (
+            <li
+              key={data.id}
+              className={`${`accumulate`} ${
+                index === item.id ? "selected" : null
+              }`}
+              //
+              onClick={() => setIndex(item.id)}
+            >
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <AnimatePresence exitBeforeEnter>
+          {data
+            .filter((item) => index === item.id)
+            .map((item) => (
+              <motion.div
+                key={item.mainid}
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                {index === item.id ? item.description : "😋"}
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </main>
+    </TabWrapper>
+  );
+}
+
+function Tab() {
+  const data = [
+    {
+      id: 0,
+      mainid: "main_0",
+      title: "적립",
+      description: <Accumulate />,
+    },
+    {
+      id: 1,
+      mainid: "main_1",
+      title: "사용",
+      description: <h2>안녕하세요 사용</h2>,
+    },
+    {
+      id: 2,
+      mainid: "main_2",
+      title: "전체",
+      description: <h2>안녕하세요 전체</h2>,
+    },
+  ];
+  const [index, setIndex] = useState(0);
+
+  return (
+    <TabWrapper>
+      <nav>
+        <ul>
+          {data.map((item) => (
+            <li
+              key={data.id}
+              className={index === item.id ? "selected" : null}
+              onClick={() => setIndex(item.id)}
+            >
+              {index === item.id ? (
+                <motion.div className="underline" layoutId="underline" />
+              ) : null}
+
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <AnimatePresence exitBeforeEnter>
+          {data
+            .filter((item) => index === item.id)
+            .map((item) => (
+              <motion.div
+                key={item.mainid}
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                {index === item.id ? item.description : "😋"}
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </main>
+    </TabWrapper>
+  );
+}
 
 function DetailPoint() {
   const [allPoint, setAllPoint] = useState(0);
@@ -68,6 +290,7 @@ function DetailPoint() {
           <p>(이달 말일 소멸)</p>
         </div>
       </Point>
+      <Tab />
     </Wrapper>
   );
 }
